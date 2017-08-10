0ff3542fd6c4bc8fd83d4ab52d7ee38bc9c25a66
onegambler <roberto.magale@gmail.com>
2017-08-10 20:53:26 +0100

    fixed requirements:

--- setup.py
+++ setup.py
@@ -2,17 +2,13 @@ from setuptools import setup
 
 setup(name='bthomehub_client',
       version='0.1',
-      description='Accessing BT Home Hub data',
+      description='A Python client that can interact with BT Home Hub routers.',
       url='https://github.com/onegambler/bthomehub_client',
       author='oneGambler',
       author_email='',
       license='MIT',
       packages=['bthomehub'],
       install_requires=[
-          'hashlib',
-          'random',
-          'threading',
-          'urllib',
           'requests'
       ],
       zip_safe=False)
