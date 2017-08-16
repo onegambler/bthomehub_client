67fcb86ab7ecfdd44bb7210ef2d1c27c2d01e786
Roberto Magale <roberto.magale@skyscanner.net>
2017-08-16 13:30:19 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='bthomehub',
       packages=["bthomehub"],
-      version='1.0.1',
+      version='1.0.2',
       description='A Python client that can interact with BT Home Hub routers.',
       url='https://github.com/onegambler/bthomehub_client',
       download_url='https://github.com/onegambler/bthomehub_client/archive/1.0.tar.gz',
